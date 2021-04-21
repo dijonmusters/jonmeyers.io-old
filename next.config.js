79a59b55600c2a37327ede3259dfcabe60c0ae66
Jon Meyers <jonathon.d.meyers@gmail.com>
2021-04-21 22:59:54 +1000

    put redirect for root back in

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const { getRedirectStatus } = require('next/dist/lib/load-custom-routes')
 module.exports = {
   async redirects() {
     return [
-      // {
-      //   source: '/',
-      //   destination: '/blog',
-      //   permanent: false,
-      // },
+      {
+        source: '/',
+        destination: '/blog',
+        permanent: false,
+      },
       {
         source: '/og',
         destination: '/api/generate-og-image',
