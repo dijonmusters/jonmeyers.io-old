4dff5c3379678de0cf70790c9d529ab563e34043
Jon Meyers <jonathon.d.meyers@gmail.com>
2021-11-15 21:34:34 +1100

    remove rss

--- pages/_document.js
+++ pages/_document.js
@@ -53,12 +53,6 @@ export default class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-          <link
-            rel="alternate"
-            type="application/rss+xml"
-            title="RSS 2.0"
-            href="/rss.xml"
-          />
         </Head>
         <body>
           <Main />
