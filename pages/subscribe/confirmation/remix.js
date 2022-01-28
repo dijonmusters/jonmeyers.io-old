000d5ada46718f7dfc27715418332916d5a47a14
Jon Meyers <jonathon.d.meyers@gmail.com>
2022-01-28 15:54:06 +1100

    add remix logo to subscription confirmation page

--- pages/subscribe/confirmation/remix.js
+++ pages/subscribe/confirmation/remix.js
@@ -2,6 +2,7 @@ import Container from 'components/Container'
 import styled from 'styled-components'
 import { lg } from 'utils/mediaQueries'
 import SEO from 'components/SEO'
+import Image from 'next/image'
 
 const Page = styled(Container)`
   display: flex;
@@ -30,6 +31,17 @@ const Text = styled.p`
   `}
 `
 
+const Logos = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Slash = styled.div`
+  margin-left: 1rem;
+  margin-right: 1rem;
+  font-size: 3rem;
+`
+
 const ConfirmationSaaS = () => {
   return (
     <>
@@ -38,6 +50,11 @@ const ConfirmationSaaS = () => {
         description="Subscribed to the upcoming Level Up Tutorials course building an app with Remix and Supabase"
       />
       <Page>
+        <Logos>
+          <Image src="/remix-logo.svg" width={100} height={100} />
+          <Slash>/</Slash>
+          <Image src="/supabase-logo.svg" width={100} height={100} />
+        </Logos>
         <Title>Thanks for signing up!</Title>
         <Text>I will let you know when the course is live!</Text>
       </Page>
